2d24cbcd17439e4017dbdb2fc37692decd1659a2
dom <limoudom2001@gmail.com>
2024-05-13 16:30:03 +1000

    fixed absolute layout for header and font size for welcome message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,13 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="headerContainer">
-      <div className="headerItems">
-        <div className="headerLeft">
-          <div className="domLogo">OUDOM LIM</div>
-        </div>
+      <div className="headerLeft">
+        <div className="domLogo">OUDOM LIM</div>
+      </div>
 
-        <div className="headerRight">
-          <div className="projectLink">Projects</div>
-          <div className="contactMeLink">Contact Me</div>
-        </div>
+      <div className="headerRight">
+        <div className="projectLink">Projects</div>
+        <div className="contactMeLink">Contact Me</div>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,25 +6,6 @@ import AnimatedBackground from "../components/AnimatedBackground";
 
 export default function HomePage() {
   return (
-    // <div className="homePageContainer">
-    //   <div className="headerContainer">
-    //     <Header />
-    //   </div>
-    //   <div className="contentContainer">
-    //     <div className="robotAndMessageContainer">
-    //       {/* <div className="homePageBio">
-    //         I am a recent UNSW graduate and a lover of learning and technology.
-    //         I've studied and worked across diverse fields ranging from AI to
-    //         Cybersecurity. Beyond tech, I'm into perfecting my craft through
-    //         BJJ, Muay Thai, Boxing, and weightlifting. I find that keeping
-    //         active helps me not only physically but also improves my mood and
-    //         mental clarity.
-    //       </div> */}
-    //       <LoadupMessage />
-    //       <Robot />
-    //     </div>
-    //   </div>
-    // </div>
     <>
       <Header />
       <AnimatedBackground />
